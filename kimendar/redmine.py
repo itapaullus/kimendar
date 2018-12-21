97c49e14f8bd9395e10f2a8870631dc327b65175
eww.paullus@gmail.com <tax_norm_f135>
2018-12-21 18:16:47 +0300

    Добавлена интеграция с Redmine

--- kimendar/redmine.py
+++ kimendar/redmine.py
@@ -1,4 +1,17 @@
 from redminelib import *
 redmine = Redmine(r"https://redmine.it-alnc.ru", username = 'i.konkin', password = '12345678')
-project = redmine.project.get('fbid_support')
-print(type(project))
+project = redmine.project.get('support')
+issues = project.issues
+
+def get_issue_url(issue):
+    return 'https://redmine.it-alnc.ru/issues/{}'.format(issue.id)
+
+def get_issue_message(issue):
+    result = '<a href="{}">{}</a>'.format(get_issue_url(issue), '#'+str(issue.id) + ' ' + issue.subject)
+    return result
+
+def get_support_info():
+    result = ''
+    for res in issues.__iter__():
+        result = result + get_issue_message(res)+'\n'
+    return result
--- kimendar/tlgrm.py
+++ kimendar/tlgrm.py
@@ -1,5 +1,7 @@
 # Настройки
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
+from kimendar import redmine
 import os
 bot_token = os.environ.get('TOKEN_TLG')
 updater = Updater(token=bot_token) # Токен API к Telegram
@@ -7,17 +9,34 @@ dispatcher = updater.dispatcher
 # Обработка команд
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Hello Dear, lets talk')
+    # bot.
     print('Start Command')
+
+def testCommand(bot, update):
+    try:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=redmine.get_support_info(),
+                         parse_mode=telegram.ParseMode.HTML)
+    except Exception as e:
+        print(e)
+    print('Test Command')
+
 def textMessage(bot, update):
     response = 'Ok sweetty: ' + update.message.text
     print('inmessage: ' + update.message.text)
     bot.send_message(chat_id=update.message.chat_id, text=response)
-# Хендлеры
+# Хендлеры команд
 start_command_handler = CommandHandler('start', startCommand)
+test_command_handler = CommandHandler('test', testCommand)
+
+# Хендлеры сообщений
 text_message_handler = MessageHandler(Filters.text, textMessage)
+
+
 # Добавляем хендлеры в диспетчер
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
+dispatcher.add_handler(test_command_handler)
 # Начинаем поиск обновлений
 updater.start_polling(clean=True)
 # Останавливаем бота, если были нажаты Ctrl + C
